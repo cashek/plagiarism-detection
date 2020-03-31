829cc063f168afc88e2039d40075da5b5fcec5be
Carl Shek <shek.carl@gmail.com>
2020-03-31 18:37:11 -0400

    Initial commit of plagiarism-detection prototype

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prototype plagiarism detection on PAN 2011 dataset',
+    author='Carl Shek',
+    license='BSD-3',
+)
